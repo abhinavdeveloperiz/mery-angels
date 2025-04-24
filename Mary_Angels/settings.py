084d59f8a47a4b2b50be46e58759f4677f6d4059
abhinavmabhi <dexmarketing192018@gmail.com>
2025-04-24 14:50:48 +0530

    second

--- Mary_Angels/settings.py
+++ Mary_Angels/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%=)ufx6w9gplgozu71g3t*@(t=iu(&9931l9%$#3owjpg!w43
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
